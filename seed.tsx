5e6925edde92f78dfeaf53974f56e9c28b4298b1
Alex Guo <aguo2@uiowa.edu>
2024-11-26 21:04:55 -0500

    added dot env

--- seed.tsx
+++ seed.tsx
@@ -2,3 +2,14 @@ import pg from 'pg'
 const { Client } = pg
 const client = new Client()
 
+try {
+    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message) // Hello world!
+ } catch (err) {
+    console.error(err);
+ } finally {
+    await client.end()
+ }
+
+
+await client.end()
