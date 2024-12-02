6e2eb12ae39ddb05e9b5c1c7c513798367570bc5
Alex Guo <aguo2@uiowa.edu>
2024-12-02 13:29:58 -0600

    added delete feature

--- src/app/components/FlashCardContainer.tsx
+++ src/app/components/FlashCardContainer.tsx
@@ -6,10 +6,34 @@ import { Flashcards } from "@/types/Flashcard"
 import { useEffect } from "react"
 import { debounce } from "lodash";
 
+async function deleteCard(id: number) {
+    const formData = new FormData();
+    formData.append('cardID', id.toString())
+    try {
+        const response = await fetch('http://localhost:3000/api/delete-flashcard', {
+            method: 'POST',
+            body: formData,
+        });
+
+        if (!response.ok) {
+            const txt = await response.text()
+            alert(txt);
+            return;
+        }
+
+        window.location.reload();
+
+    } catch (error) {
+        console.error('Error creating flashcard:', error);
+    }
+
+}
 
 export default function FlashCardContainer({ flashcards }: { flashcards: Flashcards }) {
     const [flashCardIndex, setFlashCardIndex] = useState(0)
     const [width, setWidth] = useState(window.innerWidth);
+
+    const getFlashcardID = () => (flashcards[flashCardIndex].id)
 
     const updateDimensions = () => {
         setWidth(window.innerWidth);
@@ -52,7 +76,7 @@ export default function FlashCardContainer({ flashcards }: { flashcards: Flashca
 
     return (<div className="outer-outer"><div className="outer-container">
         <div key={-1} className="cards-container">
-            {flashcards.map((flashcard,i) =>
+            {flashcards.map((flashcard, i) =>
                 <div key={i} className="cards-wrapper" style={{ transform: `translate(${getOffset()}px, 0px)` }}><FlashcardComponent flashcard={flashcard}></FlashcardComponent></div>
             )}
         </div>
@@ -60,7 +84,10 @@ export default function FlashCardContainer({ flashcards }: { flashcards: Flashca
             <button className={leftbuttonStyle} onClick={handleLeftClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="10.605" height="15.555" fill="#000000"><path d="M10.605 12.727 5.656 7.776l4.949-4.948L7.777 0 0 7.776l7.777 7.779 2.828-2.828z" /></svg>
             </button> {/* left */}
-            <button className="control-button">
+            <button className="control-button" onClick={() => {
+                const cardID = getFlashcardID()
+                deleteCard(cardID)
+            }}>
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 24 24" className="svg-icon">
                     <path d="M 10 2 L 9 3 L 5 3 C 4.4 3 4 3.4 4 4 C 4 4.6 4.4 5 5 5 L 7 5 L 17 5 L 19 5 C 19.6 5 20 4.6 20 4 C 20 3.4 19.6 3 19 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 20 C 5 21.1 5.9 22 7 22 L 17 22 C 18.1 22 19 21.1 19 20 L 19 7 L 5 7 z M 9 9 C 9.6 9 10 9.4 10 10 L 10 19 C 10 19.6 9.6 20 9 20 C 8.4 20 8 19.6 8 19 L 8 10 C 8 9.4 8.4 9 9 9 z M 15 9 C 15.6 9 16 9.4 16 10 L 16 19 C 16 19.6 15.6 20 15 20 C 14.4 20 14 19.6 14 19 L 14 10 C 14 9.4 14.4 9 15 9 z"></path>
                 </svg>
